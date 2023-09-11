dc94b7a4b7e7a58aad9341323d65d5aed4af017c
julian.campos <julian.campos.ext@infracommerce.com.br>
2023-09-11 16:31:44 -0300

    resolvendo primeiro desafio kotlin

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,52 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+data class Usuario(val name: String)
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
+data class Formacao(val nome: String, val nivel: Nivel, var conteudos: List<ConteudoEducacional>) {
     val inscritos = mutableListOf<Usuario>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricular(usuarios: List<Usuario>) {
+        for(u in usuarios) {
+	        inscritos.add(u)
+        }
+    }
+
+    fun listarInscritos() {
+        println("======================================")
+        println("Formação: ${nome} - Nivel: ${nivel}")
+        for (i in inscritos) {
+            println("Nome: ${i.name}")
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    // Cria disciplinas
+    val disciplineKotlin = ConteudoEducacional("Kotlin")
+    val disciplineAndroid = ConteudoEducacional("Android", 200)
+    val disciplineNode = ConteudoEducacional("Node", 200)
+    var disciplineJavascript = ConteudoEducacional("Javascript", 150)
+    var disciplineMongodb = ConteudoEducacional("Mongodb", 100)
+
+    // Cria conteudos
+    val conteudosKotlin = listOf<ConteudoEducacional>(disciplineKotlin, disciplineAndroid)
+    val conteudosNode = listOf<ConteudoEducacional>(disciplineNode, disciplineJavascript, disciplineMongodb)
+
+    // Cria formação
+    var formacaoKotlin = Formacao("Mobile Kotlin", Nivel.BASICO, conteudosKotlin)
+    var formacaoNode = Formacao("Backend Node", Nivel.INTERMEDIARIO, conteudosNode)
+    
+    // Cria usuario
+    var alunosKotlin: List<Usuario> = listOf(Usuario("Julian Campos"), Usuario("Marina"))
+    var alunosNode: List<Usuario> = listOf(Usuario("Alice"), Usuario("Henrique"))
+    
+    // Matricula usuarios
+    formacaoKotlin.matricular(alunosKotlin)
+    formacaoNode.matricular(alunosNode)
+    
+    // lista usuarios matriculados
+    formacaoKotlin.listarInscritos()
+    formacaoNode.listarInscritos()
 }
